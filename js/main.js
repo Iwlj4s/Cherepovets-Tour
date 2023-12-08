62d5252fea80d3cb8e4e036f084e6ca0d8a5aa85
Iwlj4s <elizavetalenn@mail.ru>
2023-12-09 00:06:25 +0300

    Good main map and some other little change

--- js/main.js
+++ js/main.js
@@ -4,53 +4,42 @@ ymaps.ready(function () {
         center: [59.089255, 37.919518],
         zoom: 15,
         controls: ['zoomControl', 'typeSelector', 'fullscreenControl']
-    }, {
-        yandexMapDisablePoiInteractivity: true // Отключение взаимодействия с объектами на карте
     });
 
-    // Маршрут
-    var myRoute = new ymaps.multiRouter.MultiRoute({
-        referencePoints: [
-            // Координаты точек маршрута
-            [59.089255, 37.919518], // Ледовый Дворец
-            [59.108317, 37.905117], // Октябрьский Мост
-            [59.120796, 37.919356], // Соляной Сад
-            [59.119752, 37.928020], // Воскресный собор
-            [59.121141, 37.932118], // Мемориальный Дом музей Верещагиных
-            [59.125495, 37.941750], // Набережная
-            [59.124632, 37.930021], // Камерный Театр
-            [59.128096, 37.922022], // Дом Музыки и Кино Комсомолец
-            [59.128416, 37.918675], // Комсомольский парк
-            [59.121428, 37.900914], // Дворец Металлургов
-            [59.133101, 37.845876], // Музей Металлургической промышленности
-            [59.129880, 37.992554] // Парк 200-Летия Череповца
-        ],
-        params: {
-            // Параметры маршрута (например, вид транспорта)
-            routingMode: 'auto'
-        },
-        options: {
-            wayPointVisible: false,
-            wayPointIconFillColor: 'blue'
-        }
-    }, {
-        // Внешний вид маршрута
-        routeStrokeWidth: 2,
-        routeStrokeColor: '0000ffff',
-        routeActiveStrokeWidth: 4,
-        routeActiveStrokeColor: 'ff0000ff'
+    // Массив с координатами и названиями объектов
+    var objects = [
+        { name: 'Ледовый Дворец', coords: [59.089363, 37.919456] },
+        { name: 'Октябрьский Мост', coords: [59.109136, 37.904910] },
+        { name: 'Соляной Сад', coords: [59.119401, 37.917570] },
+        { name: 'Воскресный собор', coords: [59.120108, 37.928687] },
+        { name: 'Мемориальный Дом музей Верещагиных', coords: [59.122125, 37.932474] },
+        { name: 'Набережная', coords: [59.125495, 37.941750] },
+        { name: 'Камерный Театр', coords: [59.124632, 37.930021] },
+        { name: 'Дом Музыки и Кино Комсомолец', coords: [59.128096, 37.922022] },
+        { name: 'Комсомольский парк', coords: [59.128416, 37.918675] },
+        { name: 'Дворец Металлургов', coords: [59.121428, 37.900914] },
+        { name: 'Музей Металлургической', coords: [59.133101, 37.845876] },
+        { name: 'Парк 200-Летия Череповца', coords: [59.129880, 37.992554] }
+    ];
+
+    // Добавление меток на карту
+    var placemarks = [];
+    for (var i = 0; i < objects.length; i++) {
+        var object = objects[i];
+        var placemark = new ymaps.Placemark(object.coords, {
+            hintContent: object.name,
+            balloonContent: object.name
+        });
+        placemarks.push(placemark);
+        main_map.geoObjects.add(placemark);
+    }
+
+    // Создание маршрута
+    ymaps.route(placemarks.map(function (placemark) {
+        return placemark.geometry.getCoordinates();
+    }), {
+        mapStateAutoApply: true
+    }).then(function (route) {
+        main_map.geoObjects.add(route);
     });
-
-    // Добавляем маршрут на карту
-    main_map.geoObjects.add(myRoute);
-
-        // Изменяем размер мини-карты при просмотре панорамы
-    myMap.container.events.add('fullscreenenter', function () {
-        myMap.container.fitToViewport();
-    });
-
-    myMap.container.events.add('fullscreenexit', function () {
-        myMap.container.fitToViewport();
-    });
-
 });
